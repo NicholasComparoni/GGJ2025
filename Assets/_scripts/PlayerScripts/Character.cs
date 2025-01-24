cabf1991d4d228e7211f9b34562c832dc8d610e2
Nicholas Comparoni <38912282+NicholasComparoni@users.noreply.github.com>
2025-01-25 00:46:02 +0100

    feat Character

---
+++ Assets/_scripts/PlayerScripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] public int health;
+    [SerializeField] protected int _movementSpeed;
+    [SerializeField] public int fireRate;
+    [SerializeField] public Rigidbody _rb;
+    [SerializeField] protected Collider _bodyCollisionIdentifier;
+}
