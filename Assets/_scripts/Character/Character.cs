46d0e82f51ce5812bfe506975613c483b714df59
Mike <mic.bellotti02@gmail.com>
2025-01-25 10:22:51 +0100

    add getcomponent on character start function

--- Assets/_scripts/Character/Character.cs
+++ Assets/_scripts/Character/Character.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Character : MonoBehaviour
 {
+    [Header("Stats")]
     [SerializeField] protected int _health;
     [SerializeField] protected int _movementSpeed;
     [SerializeField] protected int _fireRate;
-    [SerializeField] protected Rigidbody _rb;
-    [SerializeField] protected Collider _bodyCollisionIdentifier;
+    //References
+    protected Rigidbody _rb;
+    protected Collider _bodyCollisionIdentifier;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _bodyCollisionIdentifier = GetComponent<Collider>();
+    }
 }
